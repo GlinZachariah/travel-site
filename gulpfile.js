f8e501e897d97784bffe2b5aeedc258e24e82f53
GlinZachariah <glincool.zac@gmail.com>
2018-05-27 06:45:21 -0700

    added browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var postcss =require('gulp-postcss');
 var gulpimport=require('postcss-import');
 var simplevars=require('postcss-simple-vars');
 var nested =require('postcss-nested');
+var browserSync =require('browser-sync');
 
 gulp.task('default',function(){
 	console.log("This is the default gulp function");
@@ -17,8 +18,32 @@ return gulp.src('./app/assets/css/main.css')
 	.pipe(gulp.dest('./app/temp'));
 });
 
+gulp.task('reload',function(){
+	browserSync.reload();
+});
+
 gulp.task('watch',function(){
 	watch('./app/assets/**/*.css',function(){
 		gulp.start('style');
+		gulp.start('reload');
+	});
+
+	watch('./app/index.html',function(){
+		gulp.start('reload');
+	})
+
+	browserSync.init({
+		server:{
+			// notify: false;
+			baseDir:"app"
+		}
 	});
 });
+
+
+
+
+// gulp.task('cssinject',['style'],function(){
+// 	return gulp.src('./app/temp/main.css')
+// 				.pipe(browserSync.stream());
+// });
