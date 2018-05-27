8c0a0bf997f4bfc055ac27545798fa318fced21a
GlinZachariah <glincool.zac@gmail.com>
2018-05-27 21:32:14 +0530

    added css error handler

--- gulp/style.js
+++ gulp/style.js
@@ -9,6 +9,10 @@ var nested =require('postcss-nested');
 gulp.task('style',function(){
 return gulp.src('./app/assets/css/main.css')
 	.pipe(postcss([gulpimport,nested,simplevars,autoprefix]))
+	.on('error',function(errorinfo){
+		console.log(errorinfo.toString());
+		this.emit('end');
+	})
 	.pipe(gulp.dest('./app/temp'));
 });
 
